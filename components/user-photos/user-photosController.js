31145a5eeebb8babaefa7dd874203774080eaf77
karan1149 <karan1149@gmail.com>
2016-02-27 21:45:13 -0800

    Undid project 5 extra credit for simplicity.

--- components/user-photos/user-photosController.js
+++ components/user-photos/user-photosController.js
@@ -8,17 +8,7 @@ cs142App.controller('UserPhotosController', ['$scope', '$routeParams','$location
      */
     var userId = $routeParams.userId;
 
-    // get a valid photoNum
-    $scope.photoNum = 0;
-    if (Number($routeParams.photoNum) >= 0){
-      $scope.main.advanced = true;
-    }
-    if ($scope.main.advanced){
-        $scope.photoNum = Number($routeParams.photoNum);
-    }
-    if (isNaN($scope.photoNum)){
-      $scope.photoNum = 0;
-    }
+
     $scope.models = {};
     $scope.models.photos = [];
       $scope.FetchModel("/photosOfUser/" + userId + "/", function(photos){
@@ -33,27 +23,7 @@ cs142App.controller('UserPhotosController', ['$scope', '$routeParams','$location
           var lastName = $scope.models.user.last_name || "";
           $scope.main.title = firstName + " " + lastName + " Photos";
           $scope.main.context = "Photos for " + firstName + " " + lastName;
-          $scope.firstPhoto = $scope.photoNum === 0;
-          $scope.lastPhoto = $scope.photoNum === photosNum - 1;
         });
-        $scope.$watch('main.advanced', function(){
-            // set photos set to appropriate photo and return to full set if appropriate
-            if ($scope.main.advanced){
-              console.log($scope.photoNum);
-              $scope.models.photos = $scope.models.photos.slice($scope.photoNum, $scope.photoNum + 1);
-            } else {
-              $scope.models.photos = $scope.models.photos;
-            }
-        });
-      });
 
-      $scope.$watch('main.advanced', function(){
-          // set photos set to appropriate photo and return to full set if appropriate
-          if ($scope.main.advanced){
-            console.log($scope.photoNum);
-            $scope.models.photos = $scope.models.photos.slice($scope.photoNum, $scope.photoNum + 1);
-          } else {
-            $scope.models.photos = $scope.models.photos;
-          }
       });
   }]);
